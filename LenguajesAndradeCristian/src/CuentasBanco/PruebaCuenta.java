a087f5c9dde8f0b1cda3c0cfac4945be2e56d3be
rubensve <ruben_veloz89@hotmail.com>
2014-02-19 15:04:54 -0600

    Update PruebaCuenta.java

--- LenguajesAndradeCristian/src/CuentasBanco/PruebaCuenta.java
+++ LenguajesAndradeCristian/src/CuentasBanco/PruebaCuenta.java
@@ -17,7 +17,24 @@ public class PruebaCuenta {
         cuenta3.setId(7);
         cuenta3.setNombre("Andrade");
         cuenta3.setSaldo(36590);
+        
+        for (int x=1; x<2; x++)
+        {
+         if (cuenta1.getClass().equals(CuentaAhorro.class)) {
+        System.out.println("Cuenta de Ahorro creada con exito");}
+         else {
+                if (cuenta3.getClass().equals(CuentaDebito.class)) {
+                System.out.println("Cuenta de Debito creada con exito");}
+                else {
+                    if (cuenta3.getClass().equals(CuentaNomina.class)) {
+                System.out.println("Cuenta de Nomina creada con exito");}
+                     }
+               }
+        }
  
     }
 }
 
+//Solo pude hacerlo manual, no pude hacer que el numero del objeto avanzara 
+//conforme al for, lo unico que puedo hacer es ocupar el polimorfismo pero 
+//le quitaria la emocion jajaja 
